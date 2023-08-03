3faf995a692b130494966be9347a17f6e4c70fb1
liunux <liunux@office>
2023-08-03 16:27:23 +0800

    export chat history to markdown

--- streamlit_chatbox/messages.py
+++ streamlit_chatbox/messages.py
@@ -21,9 +21,12 @@ class ChatBox:
             if isinstance(greeting, str):
                 greetings[i] = Markdown(greeting)
         self._greetings = greetings
+        self.init_session()
+
+    def init_session(self):
         if self._session_key not in st.session_state:
             st.session_state[self._session_key] = {}
-            self.reset_history()
+            self.reset_history("default")
 
     def reset_history(self, name=None):
         name = name or self._chat_name
@@ -83,10 +86,21 @@ class ChatBox:
 
         return result
 
-    # def export2md(self, chat_name="default", filter=None):
-    #     lines = []
-    #     for msg in self.history:
-    #         lines.append()
+    def export2md(self, chat_name: str ="default", filter: Callable =None) -> List[str]:
+        lines = [
+            "<style> td, th {border: none!important;}</style>\n"
+            "|  |  |\n",
+            "|--|--|\n",
+        ]
+        for msg in self.history:
+            if msg["role"] == "user":
+                avatar = '''<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="eyeqlp51 css-fblp2m ex0cdmw0"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M10.25 13a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0zM15 11.75a1.25 1.25 0 100 2.5 1.25 1.25 0 000-2.5zm7 .25c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10zM10.66 4.12C12.06 6.44 14.6 8 17.5 8c.46 0 .91-.05 1.34-.12C17.44 5.56 14.9 4 12 4c-.46 0-.91.05-1.34.12zM4.42 9.47a8.046 8.046 0 003.66-4.44 8.046 8.046 0 00-3.66 4.44zM20 12c0-.78-.12-1.53-.33-2.24-.7.15-1.42.24-2.17.24a10 10 0 01-7.76-3.69A10.016 10.016 0 014 11.86c.01.04 0 .09 0 .14 0 4.41 3.59 8 8 8s8-3.59 8-8z"></path></svg>'''
+            else:
+                avatar = '''<svg viewBox="0 0 24 24" aria-hidden="true" focusable="false" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="inherit" class="eyeqlp51 css-fblp2m ex0cdmw0"><rect width="24" height="24" fill="none"></rect><path d="M20 9V7c0-1.1-.9-2-2-2h-3c0-1.66-1.34-3-3-3S9 3.34 9 5H6c-1.1 0-2 .9-2 2v2c-1.66 0-3 1.34-3 3s1.34 3 3 3v4c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-4c1.66 0 3-1.34 3-3s-1.34-3-3-3zm-2 10H6V7h12v12zm-9-6c-.83 0-1.5-.67-1.5-1.5S8.17 10 9 10s1.5.67 1.5 1.5S9.83 13 9 13zm7.5-1.5c0 .83-.67 1.5-1.5 1.5s-1.5-.67-1.5-1.5.67-1.5 1.5-1.5 1.5.67 1.5 1.5zM8 15h8v2H8v-2z"></path></svg>'''
+            content = "\n\n".join(e._content for e in msg["elements"])
+            line = f"|{avatar}|{content}|\n"
+            lines.append(line)
+        return lines
 
     def _prepare_elements(
         self,
